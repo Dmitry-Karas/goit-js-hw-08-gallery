d39e8e09ac2ed6b90e6b36d71d2c6974005e4e86
Dmitry Karas <dmitrythekaras@gmail.com>
2021-04-07 22:38:53 +0300

    added some more pictures and styles

--- gallery-items.js
+++ gallery-items.js
@@ -62,4 +62,130 @@ export default [
       'https://cdn.pixabay.com/photo/2019/05/17/04/35/lighthouse-4208843_1280.jpg',
     description: 'Lighthouse Coast Sea',
   },
+  {
+    preview:
+      'https://cdn.pixabay.com/photo/2018/03/11/14/09/eggs-3216877__340.jpg',
+    original:
+      'https://cdn.pixabay.com/photo/2018/03/11/14/09/eggs-3216877_1280.jpg',
+    description: 'Eggs',
+  },
+  {
+    preview:
+      'https://cdn.pixabay.com/photo/2021/03/16/11/31/pencils-6099511__340.jpg',
+    original:
+      'https://cdn.pixabay.com/photo/2021/03/16/11/31/pencils-6099511_1280.jpg',
+    description: 'Pencils',
+  },
+  {
+    preview:
+      'https://cdn.pixabay.com/photo/2021/04/02/19/13/sea-6145800__340.jpg',
+    original:
+      'https://cdn.pixabay.com/photo/2021/04/02/19/13/sea-6145800_1280.jpg',
+    description: 'Sea',
+  },
+  {
+    preview:
+      'https://cdn.pixabay.com/photo/2020/01/03/11/44/freedom-4737919__340.jpg',
+    original:
+      'https://cdn.pixabay.com/photo/2020/01/03/11/44/freedom-4737919_1280.jpg',
+    description: 'Freedom',
+  },
+  {
+    preview:
+      'https://cdn.pixabay.com/photo/2021/03/22/19/42/beach-6115569__340.jpg',
+    original:
+      'https://cdn.pixabay.com/photo/2021/03/22/19/42/beach-6115569_1280.jpg',
+    description: 'Beach',
+  },
+  {
+    preview:
+      'https://cdn.pixabay.com/photo/2020/12/23/11/50/lavender-5854761__340.jpg',
+    original:
+      'https://cdn.pixabay.com/photo/2020/12/23/11/50/lavender-5854761_1280.jpg',
+    description: 'Lavender',
+  },
+  {
+    preview:
+      'https://cdn.pixabay.com/photo/2020/02/09/18/55/bastei-4834233__340.jpg',
+    original:
+      'https://cdn.pixabay.com/photo/2020/02/09/18/55/bastei-4834233_1280.jpg',
+    description: 'Bastei',
+  },
+  {
+    preview:
+      'https://cdn.pixabay.com/photo/2021/03/23/06/23/flamingo-6116445__340.jpg',
+    original:
+      'https://cdn.pixabay.com/photo/2021/03/23/06/23/flamingo-6116445_1280.jpg',
+    description: 'Flamingo',
+  },
+  {
+    preview:
+      'https://cdn.pixabay.com/photo/2021/01/27/06/55/nova-scotia-duck-tolling-retriever-5953889__340.jpg',
+    original:
+      'https://cdn.pixabay.com/photo/2021/01/27/06/55/nova-scotia-duck-tolling-retriever-5953889_1280.jpg',
+    description: 'Nova scotia duck tolling retriever',
+  },
+  {
+    preview:
+      'https://cdn.pixabay.com/photo/2021/03/11/10/34/lake-6086728__340.jpg',
+    original:
+      'https://cdn.pixabay.com/photo/2021/03/11/10/34/lake-6086728_1280.jpg',
+    description: 'Lake',
+  },
+  {
+    preview:
+      'https://cdn.pixabay.com/photo/2018/09/04/10/27/learn-3653430__340.jpg',
+    original:
+      'https://cdn.pixabay.com/photo/2018/09/04/10/27/learn-3653430_1280.jpg',
+    description: 'Learn',
+  },
+  {
+    preview:
+      'https://cdn.pixabay.com/photo/2021/04/01/10/15/squirrel-6141820__340.jpg',
+    original:
+      'https://cdn.pixabay.com/photo/2021/04/01/10/15/squirrel-6141820_1280.jpg',
+    description: 'Squirrel',
+  },
+  {
+    preview:
+      'https://cdn.pixabay.com/photo/2021/03/31/02/05/fallow-deer-6138214__340.jpg',
+    original:
+      'https://cdn.pixabay.com/photo/2021/03/31/02/05/fallow-deer-6138214_1280.jpg',
+    description: 'Fallow deer',
+  },
+  {
+    preview:
+      'https://cdn.pixabay.com/photo/2021/02/06/14/24/lavanttal-5988332__340.jpg',
+    original:
+      'https://cdn.pixabay.com/photo/2021/02/06/14/24/lavanttal-5988332_1280.jpg',
+    description: 'Lavanttal',
+  },
+  {
+    preview:
+      'https://cdn.pixabay.com/photo/2021/04/03/12/37/ao-dai-6147441__340.jpg',
+    original:
+      'https://cdn.pixabay.com/photo/2021/04/03/12/37/ao-dai-6147441_1280.jpg',
+    description: 'Ao Dai',
+  },
+  {
+    preview:
+      'https://cdn.pixabay.com/photo/2021/04/03/23/28/lake-6148838__340.jpg',
+    original:
+      'https://cdn.pixabay.com/photo/2021/04/03/23/28/lake-6148838_1280.jpg',
+    description: 'Lake',
+  },
+  {
+    preview:
+      'https://cdn.pixabay.com/photo/2021/03/11/07/58/seagull-6086454__340.jpg',
+    original:
+      'https://cdn.pixabay.com/photo/2021/03/11/07/58/seagull-6086454_1280.jpg',
+    description: 'Seagull',
+  },
+  {
+    preview:
+      'https://cdn.pixabay.com/photo/2021/03/29/12/58/zebra-6134062__340.jpg',
+    original:
+      'https://cdn.pixabay.com/photo/2021/03/29/12/58/zebra-6134062_1280.jpg',
+    description: 'Zebra',
+  },
 ];
